0ef41d6dfc26e64ae5e6c93fb544b2bec11f7fb5
Salim AL-Badi <alkaisar967@gmail.com>
2021-06-07 04:59:40 -0700

    commit change

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -52,11 +52,11 @@
                     </div>
                 </header>
                 <main class="">
-                  <div class="grid mb-4 pb-10 px-8 mx-4 h-screen rounded-3xl bg-gray-100 border-4 border-green-400">
+                  <div class="grid mb-4 pb-10 px-8 mx-4 h-full rounded-3xl bg-gray-100 border-4 border-green-400 ">
   
                       <div class="grid grid-cols-12 gap-6">
                           <div class="grid grid-cols-12 col-span-12 gap-6 xxl:col-span-9">
-                              <div class="col-span-12 mt-8"> 
+                              <div class="col-span-12 mt-8 h-screen"> 
                         @yield('content')
                               </div>
                           </div>
