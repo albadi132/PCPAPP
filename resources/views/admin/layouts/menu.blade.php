e8d35c40239b310addef04299d0b82546638c31f
Salim AL-Badi <alkaisar967@gmail.com>
2021-06-07 05:29:22 -0700

    commit change

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -54,7 +54,14 @@
               <span><a href="{{ route('problems-view') }}">programming problems</a></span>
             </div>
           </li>
-         
+          <li class=" relative px-2 py-1 inline-flex items-center w-full text-sm font-semibold text-white cursor-pointer transition-colors duration-150 hover:text-red-500">
+            
+              <div class="mr-4 my-auto text-red-400">
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+              </div>
+              <span><a href="{{ route('home') }}">Logout</a></span>
+            
+            </li>
         </ul>
       </div>
     </div>
