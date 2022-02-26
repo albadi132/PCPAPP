3d477f994fc585193aa14eed06671aacfe436db8
Salim AL-Badi <alkaisar967@gmail.com>
2022-02-26 01:44:34 -0800

    test sandbox

--- app/Http/Controllers/judge/JudgeController.php
+++ app/Http/Controllers/judge/JudgeController.php
@@ -257,7 +257,8 @@ class JudgeController extends Controller
 
                                             $path2 =  $NewSubmitName;
                                             $firejailpath =  $NewCompilerName;
-                                            $command = 'firejail '.$path.' /usr/bin/g++ -o ' . $firejailpath . ' ' .  $path2;
+                                           // $command = 'firejail '.$path.' /usr/bin/g++ -o ' . $firejailpath . ' ' .  $path2;
+                                           $command = '/usr/bin/g++ -o ' . $source . ' ' .  $copypath;
                                             //dd($command);
                                             $output = null;
                                             $failed = 0;
